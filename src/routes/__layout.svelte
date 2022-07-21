50208d2206af9ccaed97024c0e240b7f0b0cd096
Eunjae Lee <hey@eunjae.dev>
2022-07-21 17:02:47 +0200

    chore: add sentry

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,17 @@
 <script>
+	import * as Sentry from '@sentry/browser';
+	import { BrowserTracing } from '@sentry/tracing';
 	import '../app.css';
+
+	Sentry.init({
+		dsn: 'https://f86c9a88f7de4626aa0f5f9295c10417@o401874.ingest.sentry.io/6591315',
+		integrations: [new BrowserTracing()],
+
+		// Set tracesSampleRate to 1.0 to capture 100%
+		// of transactions for performance monitoring.
+		// We recommend adjusting this value in production
+		tracesSampleRate: 1.0,
+	});
 </script>
 
 <slot />
