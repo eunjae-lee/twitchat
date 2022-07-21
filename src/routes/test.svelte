3cf7221bc29a3754d00b71aeda570c09fb1612ff
Eunjae Lee <hey@eunjae.dev>
2022-07-21 17:35:13 +0200

    feat: add supabase auth helpers (#8)
    * feat: add supabase auth helpers
    
    * chore: add .env.example

---
+++ src/routes/test.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { session } from '$app/stores';
+</script>
+
+{#if !$session.user}
+	<h1>I am not logged in</h1>
+{:else}
+	<h1>Welcome {$session.user.email}</h1>
+	<p>I am logged in!</p>
+{/if}
