b083c18db94887afd69628bc7a7eea651419e622
Eunjae Lee <hey@eunjae.dev>
2022-07-22 18:06:21 +0200

    feat: call setContext at layout to share lang across the app (#13)

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,13 @@
 // src/lib/db.ts
 import { createSupabaseClient } from '@supabase/auth-helpers-sveltekit';
 
-const { supabaseClient } = createSupabaseClient(
+const result = createSupabaseClient(
 	import.meta.env.VITE_SUPABASE_URL as string,
 	import.meta.env.VITE_SUPABASE_ANON_KEY as string
 );
+
+// result.supabaseClient is undefined only when the two parameters are not given above.
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const supabaseClient = result.supabaseClient!;
 
 export { supabaseClient };
