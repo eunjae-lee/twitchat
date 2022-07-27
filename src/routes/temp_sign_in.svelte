f42bba44239cdc87bfecd141dcc708740230b39c
Eunjae Lee <hey@eunjae.dev>
2022-07-27 17:32:10 +0200

    feat: add temporary login method

---
+++ src/routes/temp_sign_in.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	import { supabase } from '$lib/db';
+
+	let email: string;
+	let password: string;
+
+	async function signIn() {
+		await supabase.auth.signIn({
+			email,
+			password,
+		});
+		goto('/new');
+	}
+</script>
+
+<input type="email" bind:value={email} />
+<input type="password" bind:value={password} />
+<button on:click={signIn}>Sign In</button>
