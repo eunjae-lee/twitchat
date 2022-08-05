32c20667f965dd2aa829c0427a6e2207e5a3e8df
Eunjae Lee <hey@eunjae.dev>
2022-08-05 17:55:14 +0200

    fix(types): fix  type

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,20 +5,22 @@ import type { Lang } from '$lib/types';
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {
-	interface UserSession {
-		user: import('@supabase/supabase-js').User;
-		accessToken?: string;
-		lang: Lang;
+declare global {
+	declare namespace App {
+		interface UserSession {
+			user: import('@supabase/supabase-js').User;
+			accessToken?: string;
+			lang: Lang;
+		}
+
+		interface Locals extends UserSession {
+			error: import('@supabase/supabase-js').ApiError;
+		}
+
+		// eslint-disable-next-line @typescript-eslint/no-empty-interface
+		interface Session extends UserSession {}
+
+		// interface Platform {}
+		// interface Stuff {}
 	}
-
-	interface Locals extends UserSession {
-		error: import('@supabase/supabase-js').ApiError;
-	}
-
-	// eslint-disable-next-line @typescript-eslint/no-empty-interface
-	interface Session extends UserSession {}
-
-	// interface Platform {}
-	// interface Stuff {}
 }
