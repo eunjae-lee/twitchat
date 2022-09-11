b42c063d4d992fbeeb8c0ee93a116c414bf643ee
Eunjae Lee <hey@eunjae.dev>
2022-09-11 23:38:40 +0200

    revert: "fix: add error handling for authentication"
    This reverts commit fc43fdafd30ec46a481178437f8c7a54d59a4b6b.

--- src/routes/authenticated.svelte
+++ src/routes/authenticated.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import * as Sentry from '@sentry/browser';
 	import { goto } from '$app/navigation';
 	import { session } from '$app/stores';
 	import { popRedirectionAfterSignIn } from '$lib/auth';
@@ -17,19 +16,6 @@
 		const sessionExists = $session.user && $session.user.id;
 
 		if (sessionExists && beingRedirectedFromTwitter) {
-			const redirectTo = popRedirectionAfterSignIn();
-			if (redirectTo && redirectTo.startsWith('/')) {
-				goto(redirectTo);
-			}
-		} else {
-			Sentry.captureEvent({
-				message: 'authenticated malfunctioning',
-				extra: {
-					sessionExists,
-					beingRedirectedFromTwitter,
-					hash: window.location.hash,
-				},
-			});
 			const redirectTo = popRedirectionAfterSignIn();
 			if (redirectTo && redirectTo.startsWith('/')) {
 				goto(redirectTo);
